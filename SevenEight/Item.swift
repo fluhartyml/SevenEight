8bfc2e1795ee45aac564ecaefeb7656db7f2ed3c
Michael Fluharty <michael.fluharty@mac.com>
2025-10-18 08:45:53 -0500

    Initial Commit

---
+++ SevenEight/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SevenEight
+//
+//  Created by Michael Fluharty on 10/18/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SevenEightTests/SevenEightTests.swift
@@ -0,0 +1,16 @@
+//
+//  SevenEightTests.swift
+//  SevenEightTests
+//
+//  Created by Michael Fluharty on 10/18/25.
+//
+
+import Testing
+
+struct SevenEightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
